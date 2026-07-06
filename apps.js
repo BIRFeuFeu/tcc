c9dabd65c48e9459cf4fc433241859d74f89613d
BIRFeuFeu <alfeu.paula@escola.pr.gov.br>
2026-07-06 12:03:27 -0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -7,3 +7,34 @@ hamburger.addEventListener("click", () => {
 
 });
 
+
+window.addEventListener("scroll",()=>{
+
+const navbar=document.querySelector(".navbar");
+
+navbar.classList.toggle("scrolled",window.scrollY>40);
+
+});
+
+
+const reveals=document.querySelectorAll(".reveal");
+
+function reveal(){
+
+reveals.forEach(card=>{
+
+const top=card.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+card.classList.add("active");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll",reveal);
+
+reveal();
